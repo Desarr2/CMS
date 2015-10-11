02f199f49ac646114874135a9a8ea834902d9555
Alexandra Albarracin Fernandez <Alexandra.albarracin@ucsp.edu.pe>
2015-10-11 11:34:33 -0500

    #8 Recuperacion de Cuenta (Contraseña)

---
+++ app/forms.py
@@ -0,0 +1,16 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField, PasswordField, TextField
+from wtforms.validators import Required, DataRequired, Email, EqualTo
+
+class RecoverPassForm(Form):
+	email = TextField('Email Address', [Email(), Required(message='Forgot your email address?')])
+
+class ResetPasswordSubmit(Form):
+	password = PasswordField('Password', validators = [DataRequired()])
+	confirm = PasswordField('Confirm Password')
+
+	class CreateUserForm(Form):
+		id = StringField('id', validators=[DataRequired()])
+		nickname = StringField('nickname', validators=[DataRequired()])
+		email = StringField('email', validators=[DataRequired()])
+		password = PasswordField('pass', validators=[DataRequired()])
