3acdd3f505754181f9c1dc194faed76f7b0eb2a8
JnicolasJ <jose.jimenez@ucsp.edu.pe>
2015-10-15 20:39:02 -0500

    #20 #26 issue compeltados en colaboracion con fronimo

---
+++ app/sections/controllers.py
@@ -0,0 +1,31 @@
+from flask import (
+    Blueprint,
+    request,
+    render_template,
+    flash,
+    g,
+    session,
+    redirect,
+    url_for
+)
+
+from app import app, db
+from app.sections.forms import CreateSectionForm
+from app.sections.models import Sections
+
+
+mod_sec = Blueprint('sec', __name__, url_prefix='/sec')
+
+
+@mod_sec.route('/create_sections/', methods=['GET', 'POST'])
+def create_section():
+    form = CreateSectionForm(request.form)
+    if form.validate_on_submit():
+        section = Sections(form.section.data, form.description.data)
+        db.session.add(section)
+        db.session.commit()
+        flash("sections created")
+        return render_template("sections/create_sections.html",form = form)
+        
+
+    return render_template("sections/create_sections.html",form = form)
