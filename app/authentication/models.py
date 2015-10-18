4576bc788e337ad683a860ab5ae65193b6f34382
rdelgador <ricardo.delgado@ucsp.edu.pe>
2015-10-18 12:14:30 -0500

    Branch Sprint 1

--- app/authentication/models.py
+++ app/authentication/models.py
@@ -1,33 +1,51 @@
+import datetime
 from app import db
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+from flask import session, redirect, current_app, request
+from werkzeug import generate_password_hash, check_password_hash
 
-
-class Base(db.Model):
-
-    __abstract__ = True
-
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime,
-                              default=db.func.current_timestamp(),
-                              onupdate=db.func.current_timestamp())
-
-
-class User(Base):
+class User(db.Model):
 
     __tablename__ = 'auth_user'
 
-    username = db.Column(db.String(128),  nullable=False)
-    email = db.Column(db.String(128),  nullable=False,
-                      unique=True)
-    password = db.Column(db.String(192),  nullable=False)
-    role = db.Column(db.SmallInteger, nullable=False)
-    status = db.Column(db.SmallInteger, nullable=False)
+    id      = db.Column('id', db.Integer, primary_key=True, \
+                autoincrement='ignore_fk')
+    username = db.Column(db.String(128),  nullable=False, unique=True)
+    email   = db.Column(db.String(128),  nullable=False, unique=True)
+    password = db.Column(db.String(128),  nullable=False)
+    role    = db.Column(db.SmallInteger, nullable=False)
+    status   = db.Column(db.SmallInteger, nullable=False)
 
-    def __init__(self, name, email, password):
 
-        self.name = name
-        self.email = email
-        self.password = password
+    def __init__(self, username, email, password,role,status):
+
+        self.username = username.title()
+        self.email  = email.lower()
+        self.password = generate_password_hash(password)
+        self.role   = role
+        self.status   = status
+
+    def get_token(self, expiration=1800):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'user': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_token_email(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        id = data.get('user')
+        if id:
+            return User.query.get(id)
+        return None
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2
+        except NameError:
+            return str(self.id)  # python 3
 
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return '<User %r>' % (self.username)
