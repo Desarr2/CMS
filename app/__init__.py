1f2a2f6c2ff2db4f584a5c7aa44279640deba6a0
rdelgador <ricardo.delgado@ucsp.edu.pe>
2015-10-18 12:18:31 -0500

    Branch Sprint 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 # Import flask and template operators
 from flask import Flask, render_template
+from flask_mail import Mail
 
 # Import SQLAlchemy
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -25,14 +26,29 @@ Bower(app)
 def not_found(error):
     return render_template('404.html'), 404
 
+
 # Import a module / component using its blueprint handler variable (mod_auth)
-from app.authentication.controllers import mod_auth as auth_module
+
+
+db.create_all()
+
+app.config['MAIL_SERVER'] = 'evop5.areserver.net'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'pruebas.cms@asacoop.com'
+app.config['MAIL_PASSWORD'] = 'admin1234'
+mail=Mail(app)
+
+# Import a module / component using its blueprint handler variable (mod_auth)
+from app.sections.controllers import mod_sec as sec_module
+
+db.create_all()
+
 
 # Register blueprint(s)
-app.register_blueprint(auth_module)
+app.register_blueprint(sec_module)
 # app.register_blueprint(xyz_module)
 # ..
 
 # Build the database:
 # This will create the database file using SQLAlchemy
-db.create_all()
